c0f986d8a1ffd33390f07b1abe0eee1d548b479d
Paweloz <pawel.przybyla.1992@o2.pl>
2021-03-10 15:07:12 +0100

    6.2 Unit testing for class Calculator

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,7 +1,36 @@
 package com.kodilla.testing;
+import com.kodilla.testing.calculator.Calculator;
+import com.kodilla.testing.user.SimpleUser;
+
 
 public class TestingMain {
     public static void main(String[] args){
-        System.out.println("Moduł 6. Wprowadzenie to testowania oprogramowania");
+        SimpleUser user =new SimpleUser("theForumUser");
+        String result = user.getUserName();
+
+        if(result.equals("theForumUser")) {
+            System.out.println("test OK");
+        } else{
+            System.out.println("ERROR!");
+        }
+        //Test jednostkowy klasy calculator
+        System.out.println("Test - pierwszy test jednostkowy:");
+        Calculator calc = new Calculator();
+        int a=5;
+        int b=3;
+        int sum = calc.add(a,b);
+        int sub = calc.substract(a,b);
+        //Test add()
+        if(a+b==sum){
+            System.out.println("Metod add() : Test OK");
+        }else{
+            System.out.println("Metod add() : ERROR!");
+        }
+        //Test substract()
+        if(a-b==sub){
+            System.out.println("Metod substract() : Test OK");
+        }else{
+            System.out.println("Metod substract() : ERROR!");
+        }
     }
 }
