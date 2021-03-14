80ad215a5576f43aee403766e2280357e1b53685
Paweloz <pawel.przybyla.1992@o2.pl>
2021-03-14 17:02:59 +0100

    6.4 Average and Median metods for WeatherForecast

---
+++ kodilla-testing/src/main/java/com/kodilla/testing/weather/stub/WeatherForecast.java
@@ -0,0 +1,46 @@
+package com.kodilla.testing.weather.stub;
+
+import java.util.*;
+
+public class WeatherForecast {
+    private final Temperatures temperatures;
+
+    public WeatherForecast(Temperatures temperatures) {
+        this.temperatures = temperatures;
+    }
+
+    public Map<String, Double> calculateForecast() {
+        Map<String, Double> resultMap = new HashMap<>();
+
+        for (Map.Entry<String, Double> temperature :
+                temperatures.getTemperatures().entrySet()) {
+
+            // adding 1 celsius degree to current value
+            // as a temporary weather forecast
+            resultMap.put(temperature.getKey(), temperature.getValue() + 1.0); // [1]
+        }
+        return resultMap;
+    }
+
+    public double averageTemp(){
+        double sum=0;
+        double average;
+
+        for(Map.Entry<String,Double> entry : temperatures.getTemperatures().entrySet()){
+            sum += entry.getValue();
+        }
+        return average = sum/temperatures.getTemperatures().size();
+    }
+
+    public double medianTemp(){
+        Double[] temps = temperatures.getTemperatures().values().toArray(new Double[0]);
+        double median;
+        Arrays.sort(temps);
+        if(temps.length%2 == 1){
+            median = temps[temps.length/2];
+        }else{
+            median = (temps[temps.length/2-1]+temps[temps.length/2])/2;
+        }
+        return median;
+    }
+}
