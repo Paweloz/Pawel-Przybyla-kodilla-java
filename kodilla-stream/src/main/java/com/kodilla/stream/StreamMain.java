e54b4954131962a112c6b713468f60701aec9da5
Paweloz <pawel.przybyla.1992@o2.pl>
2021-03-17 20:26:43 +0100

    7.1 lambda for string beautifier

---
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -0,0 +1,24 @@
+package com.kodilla.stream;
+
+import com.kodilla.stream.beautifier.PoemBeautifier;
+import com.kodilla.stream.beautifier.PoemDecorator;
+
+public class StreamMain {
+    public static void main(String[] args) {
+        PoemBeautifier poemBeautifier = new PoemBeautifier();
+        String text1 ="ala ma kota";
+        String text2 ="a kot ma kaca";
+        String text3 = "to be or not to be";
+        String text4 ="Kamil Slimak";
+
+        PoemDecorator reverse = (text) -> {
+            StringBuilder output = new StringBuilder(text4).reverse();
+            return output.toString();
+        };
+
+        poemBeautifier.beautify(text1,(text) -> "ABC "+text+" ABC");
+        poemBeautifier.beautify(text2,(text) -> text2.toUpperCase());
+        poemBeautifier.beautify(text3,(text) -> text3.replace("t","T"));
+        poemBeautifier.beautify(text4,reverse);
+    }
+}
