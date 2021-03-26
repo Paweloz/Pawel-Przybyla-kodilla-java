011275ee441a0be29494c3f1b554b178c620c44d
Paweloz <pawel.przybyla.1992@o2.pl>
2021-03-26 17:50:41 +0100

    7.3 test for handling exceptions

---
+++ kodilla-exception/src/test/java/com/kodilla/exception/test/SecondChallangeTestSuite.java
@@ -0,0 +1,24 @@
+package com.kodilla.exception.test;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class SecondChallangeTestSuite {
+
+    @Test
+    void testProbablyIWillThrowException(){
+        //Given
+        SecondChallenge secondChallenge = new SecondChallenge();
+        //When & Then
+        assertAll(
+                ()->assertThrows(Exception.class,()-> secondChallenge.probablyIWillThrowException(2,1.6)),
+                ()->assertThrows(Exception.class,()->secondChallenge.probablyIWillThrowException(2.1,1.5)),
+                ()->assertThrows(Exception.class,()->secondChallenge.probablyIWillThrowException(1.1,1.5)),
+                ()->assertThrows(Exception.class,()->secondChallenge.probablyIWillThrowException(0.9,1.4)),
+                ()->assertDoesNotThrow(()->secondChallenge.probablyIWillThrowException(1.1,1.6)),
+                ()->assertDoesNotThrow(()->secondChallenge.probablyIWillThrowException(1.9,1.4)),
+                ()->assertDoesNotThrow(()->secondChallenge.probablyIWillThrowException(1.9,1.6))
+        );
+    }
+}
