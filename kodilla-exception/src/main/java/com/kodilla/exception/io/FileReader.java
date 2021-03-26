1bbda35815d0f44d4dd0f737c61f6109473d3dc0
Paweloz <pawel.przybyla.1992@o2.pl>
2021-03-26 15:53:59 +0100

    8.2 try catch finaly

--- kodilla-exception/src/main/java/com/kodilla/exception/io/FileReader.java
+++ kodilla-exception/src/main/java/com/kodilla/exception/io/FileReader.java
@@ -9,11 +9,16 @@ import java.util.stream.Stream;
 
 public class FileReader {
 
-    public void readFile() throws IOException {
+    public void readFile() {
         ClassLoader classLoader = getClass().getClassLoader();
         File file = new File(classLoader.getResource("names.txt").getFile());
-        Path path = Paths.get(file.getPath());
-        Stream<String> fileLines = Files.lines(path);
+        try(Stream<String> fileLines = Files.lines(Paths.get(file.getPath()))){
+            fileLines.forEach(System.out::println);
+        }catch (IOException e){
+            System.out.println("Nie ma pliku " +e);
+        }finally {
+            System.out.println("A komu to potrzebne ?");
+        }
 
         System.out.println(file.getPath());
     }
