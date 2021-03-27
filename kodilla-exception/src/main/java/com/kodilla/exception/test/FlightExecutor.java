5054468b811933f59cd3fe6c84d9ee4b62d9447d
Paweloz <pawel.przybyla.1992@o2.pl>
2021-03-27 19:17:27 +0100

    8.4 RouteNotFoundException

--- kodilla-exception/src/main/java/com/kodilla/exception/test/FlightExecutor.java
+++ kodilla-exception/src/main/java/com/kodilla/exception/test/FlightExecutor.java
@@ -1,11 +1,16 @@
 package com.kodilla.exception.test;
 
+import java.util.Optional;
+
 public class FlightExecutor {
 
     public static void main(String[] args) {
         FlightBrowser flightBrowser = new FlightBrowser();
         Flight flight = new Flight("Hamburg", "Sosnowiec");
 
+        //Sprawdzenie czy obiekt Flight został poprawnie zainicjowany
+        Optional<Flight> optionalFlight = Optional.ofNullable(flight);
+        flight = optionalFlight.orElse(new Flight("",""));
         try{
             flightBrowser.findFlight(flight);
         }catch(RouteNotFoundException e){
