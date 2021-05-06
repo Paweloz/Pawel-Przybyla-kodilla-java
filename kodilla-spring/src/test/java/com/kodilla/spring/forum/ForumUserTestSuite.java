2033e3f62faeec6e8cea1ce0045732afccb52641
Paweloz <pawel.przybyla.1992@o2.pl>
2021-05-06 17:16:30 +0200

    14.1 ForumUser bean

---
+++ kodilla-spring/src/test/java/com/kodilla/spring/forum/ForumUserTestSuite.java
@@ -0,0 +1,23 @@
+package com.kodilla.spring.forum;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+@SpringBootTest
+class ForumUserTestSuite {
+
+    @Test
+    void testGetUsername() {
+        //Given
+        ApplicationContext context =
+                new AnnotationConfigApplicationContext("com.kodilla.spring.forum");
+        ForumUser forumUser = context.getBean(ForumUser.class);
+        //When
+        String userName = forumUser.getUsername();
+        //Then
+        Assertions.assertEquals("John Smith", userName);
+    }
+}
