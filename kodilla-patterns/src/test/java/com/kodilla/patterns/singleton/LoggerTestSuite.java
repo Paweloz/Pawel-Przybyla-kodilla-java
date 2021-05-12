3b3e03ebe6a978ad42345b0b5584972af8a6d840
Paweloz <pawel.przybyla.1992@o2.pl>
2021-05-12 19:07:35 +0200

    "15.1 Singleton Logger"

---
+++ kodilla-patterns/src/test/java/com/kodilla/patterns/singleton/LoggerTestSuite.java
@@ -0,0 +1,18 @@
+package com.kodilla.patterns.singleton;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class LoggerTestSuite {
+
+    @Test
+    void getLastLog() {
+        //Given
+        Logger logger = Logger.INSTANCE;
+        //When
+        logger.log("some text");
+        String result = logger.getLastLog();
+        //Then
+        Assertions.assertEquals("some text", result);
+    }
+}
